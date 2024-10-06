15259736ed7b02bda7dacdbf7aed8bf3c9b62587
roosartwork <91888947+roosartwork@users.noreply.github.com>
2024-10-06 23:15:17 +0200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // Add sticky class when scrolled past header
             function updateHeader() {
-                if (window.scrollY > 10%) {
+                if (window.scrollY > 5%) {
                     header.classList.add('fixed');
                     logo.src = stickySrc;
                 } else {
